dd738805f467016f89b3d99a19dd2c31098f4906
EBosch14 <enzo.jbosch@gmail.com>
2023-06-22 22:41:43 -0300

    finish day 2

--- server/src/routes/dogs/breeds.js
+++ server/src/routes/dogs/breeds.js
@@ -6,21 +6,27 @@ const { API_URL, API_KEY } = process.env;
 const breedsRouter = Router();
 
 breedsRouter.get("/", async (req, res) => {
+  const { search } = req.query;
   try {
-    const { data } = await axios.get(`${API_URL}/breeds`);
-    res.status(200).json(data);
+    if (!search) {
+      const { data } = await axios.get(`${API_URL}/breeds`);
+      res.status(200).json(data);
+    } else {
+      const { data } = await axios.get(`${API_URL}/breeds/search?q=${search}`);
+      res.status(200).json(data);
+    }
   } catch (error) {
-    res.status(404).json({ error });
+    res.status(404).json({ error: error.message });
   }
 });
 
 breedsRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const {data} = await axios.get(`${API_URL}/breeds/${id}`)
+    const { data } = await axios.get(`${API_URL}/breeds/${id}`);
     res.status(200).json(data);
   } catch (error) {
-    res.status(404).json(error)
+    res.status(404).json({ error: error.message });
   }
 });
 
