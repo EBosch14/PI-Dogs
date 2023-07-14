74d135fe6993c2bf40d7271a123f192fd61d45c4
EBosch14 <enzo.jbosch@gmail.com>
2023-07-14 05:55:22 -0300

    fix: :bug: fix import

--- client/src/assets/components/screenLoading/index.jsx
+++ client/src/assets/components/screenLoading/index.jsx
@@ -1,10 +1,9 @@
-import loadingGIF from "/doggie-waiting.gif";
 import s from "./screenLoading.module.css"
 
 export default function ScreenLoading() {
   return (
     <div className={s.container}>
-      <img className={s.image} src={loadingGIF} alt="doggie-waiting" />
+      <img className={s.image} src="/doggie-waiting.gif" alt="doggie-waiting" />
       <p className={s.textWaiting}>Waiting for the puppies 🐶</p>
     </div>
   );
